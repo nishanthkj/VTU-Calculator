22eb6c344101b68a18132dbff5d1cccce894abd4
nishanthkj <nishanthkj12@gmail.com>
2024-09-09 19:46:30 +0530

    .

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -78,7 +78,7 @@ function Navbar() {
         >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {[
-              { name: "Home", href: "/VTU-Calculator" },
+              { name: "Home", href: "/" },
               {
                 name: "Results",
                  href: "https://results.vtu.ac.in/index.php",
