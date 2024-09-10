e1b2ca239884952deb613488b59bccd6a0a801af
nishanthkj <nishanthkj12@gmail.com>
2024-09-10 09:26:47 +0530

    Version 1.5

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -41,12 +41,12 @@ function Navbar() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="" className="h-8" alt="" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             VTU2021 {"N<"}<p>Calculator</p>
           </span>
-        </a>
+        </Link>
         <button
           onClick={handleToggle}
           data-collapse-toggle="navbar-default"
