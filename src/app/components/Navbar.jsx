6ab6303fad2b1c807c5c048d907aa77edc9f1ff8
nishanthkj <nishanthkj12@gmail.com>
2024-09-11 11:38:00 +0530

    Version 1.7

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -41,7 +41,7 @@ function Navbar() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="/VTU-Calculator/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="" className="h-8" alt="" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             VTU2021 {"N<"}<p>Calculator</p>
